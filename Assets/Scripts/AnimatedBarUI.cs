b7b4129d8a11218a20fb6b2f16352d82e7c039e9
matthew0313 <85425949+matthew0313@users.noreply.github.com>
2025-07-08 04:00:46 +0900

    r

---
+++ Assets/Scripts/AnimatedBarUI.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedBarUI : MonoBehaviour
+{
+    [SerializeField] Image bar, reduceBar, refillBar;
+    [SerializeField] float lerpRate = 3.0f;
+    [SerializeField] float waitTime = 1.0f;
+    [SerializeField] float m_targetScale = 1.0f;
+    public float targetScale
+    {
+        get => m_targetScale;
+        set
+        {
+            m_targetScale = value;
+            UpdateBar();
+        }
+    }
+    float currentScale = 1.0f;
+    private void Update()
+    {
+        currentScale = Mathf.Lerp(currentScale, targetScale, lerpRate * Time.deltaTime);
+        UpdateBar();
+    }
+    void UpdateBar()
+    {
+        if (currentScale < targetScale)
+        {
+            reduceBar.transform.localScale = new Vector2(0.0f, 1.0f);
+            refillBar.transform.localScale = new Vector2(targetScale, 1.0f);
+            bar.transform.localScale = new Vector2(currentScale, 1.0f);
+        }
+        else
+        {
+            refillBar.transform.localScale = new Vector2(0.0f, 1.0f);
+            bar.transform.localScale = new Vector2(targetScale, 1.0f);
+            reduceBar.transform.localScale = new Vector2(currentScale, 1.0f);
+        }
+    }
+}
